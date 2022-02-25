762ee5f0ce0a20a85f2adf569031a6a8affa04c2
Ubuntu <ubuntu@ip-172-31-88-101.ec2.internal>
2022-02-25 23:15:04 +0000

    fixing bugs

--- core.py
+++ core.py
@@ -2,11 +2,11 @@
 """
 @author: HARSHIT
 """
-# import os
-# import numpy as np
-# import pandas as pd
-# import joblib
-# from sklearn.ensemble import RandomForestClassifier
+import os
+import numpy as np
+import pandas as pd
+import joblib
+from sklearn.ensemble import RandomForestClassifier
 
 
 # def model_train():
@@ -124,148 +124,148 @@
 #     #     print("Feature %s : (%f)" %(x.columns[f], model.feature_importances_[f]))
         
         
-# def model_predict(user_data):
-#     # load, no need to initialize the loaded_rf
-#     print("Inside Model Predict")
-#     loaded_model = joblib.load("./rf_model.joblib")
-#     prediction = loaded_model.predict(user_data)
-#     return prediction        
+def model_predict(user_data):
+    # load, no need to initialize the loaded_rf
+    print("Inside Model Predict")
+    loaded_model = joblib.load("./new_rf_model.joblib")
+    prediction = loaded_model.predict(user_data)
+    return prediction        
         
         
 
         
 ###### - Old Code
-import os
-import numpy as np
-import pandas as pd
-import joblib
-from sklearn.ensemble import RandomForestClassifier
+# import os
+# import numpy as np
+# import pandas as pd
+# import joblib
+# from sklearn.ensemble import RandomForestClassifier
 
 
-def model_train():
-    # to_predict = user_data
-    df = pd.read_csv('survey.csv')
-    df.drop(df.columns[[0, 4, 5, 26]], axis=1, inplace=True)
-    # Deleting Timestamp, State, self-employed and comments
+# def model_train():
+#     # to_predict = user_data
+#     df = pd.read_csv('survey.csv')
+#     df.drop(df.columns[[0, 4, 5, 26]], axis=1, inplace=True)
+#     # Deleting Timestamp, State, self-employed and comments
 
-    df['Gender'] = df['Gender'].str.lower()
+#     df['Gender'] = df['Gender'].str.lower()
 
-    df.Gender = df.Gender.replace('m', 'male')
-    df.Gender = df.Gender.replace('male-ish', 'male')
-    df.Gender = df.Gender.replace('maile', 'male')
-    df.Gender = df.Gender.replace('mal', 'male')
-    df.Gender = df.Gender.replace('male (cis)', 'male')
-    df.Gender = df.Gender.replace('make', 'male')
-    df.Gender = df.Gender.replace('man', 'male')
-    df.Gender = df.Gender.replace('msle', 'male')
-    df.Gender = df.Gender.replace('mail', 'male')
-    df.Gender = df.Gender.replace('malr', 'male')
-    df.Gender = df.Gender.replace('cis man', 'male')
-    df.Gender = df.Gender.replace('cis male', 'male')
-    df.Gender = df.Gender.replace('male', 'male')
-    df.Gender = df.Gender.replace('male ', 'male')
+#     df.Gender = df.Gender.replace('m', 'male')
+#     df.Gender = df.Gender.replace('male-ish', 'male')
+#     df.Gender = df.Gender.replace('maile', 'male')
+#     df.Gender = df.Gender.replace('mal', 'male')
+#     df.Gender = df.Gender.replace('male (cis)', 'male')
+#     df.Gender = df.Gender.replace('make', 'male')
+#     df.Gender = df.Gender.replace('man', 'male')
+#     df.Gender = df.Gender.replace('msle', 'male')
+#     df.Gender = df.Gender.replace('mail', 'male')
+#     df.Gender = df.Gender.replace('malr', 'male')
+#     df.Gender = df.Gender.replace('cis man', 'male')
+#     df.Gender = df.Gender.replace('cis male', 'male')
+#     df.Gender = df.Gender.replace('male', 'male')
+#     df.Gender = df.Gender.replace('male ', 'male')
 
-    df.Gender = df.Gender.replace('f', 'female')
-    df.Gender = df.Gender.replace('cis female', 'female')
-    df.Gender = df.Gender.replace('woman', 'female')
-    df.Gender = df.Gender.replace('femake', 'female')
-    df.Gender = df.Gender.replace('female ', 'female')
-    df.Gender = df.Gender.replace('cis-female/femme', 'female')
-    df.Gender = df.Gender.replace('female (cis)', 'female')
-    df.Gender = df.Gender.replace('femail', 'female')
+#     df.Gender = df.Gender.replace('f', 'female')
+#     df.Gender = df.Gender.replace('cis female', 'female')
+#     df.Gender = df.Gender.replace('woman', 'female')
+#     df.Gender = df.Gender.replace('femake', 'female')
+#     df.Gender = df.Gender.replace('female ', 'female')
+#     df.Gender = df.Gender.replace('cis-female/femme', 'female')
+#     df.Gender = df.Gender.replace('female (cis)', 'female')
+#     df.Gender = df.Gender.replace('femail', 'female')
 
-    df.Gender = df.Gender.replace('trans-female', 'trans')
-    df.Gender = df.Gender.replace('something kinda male?', 'trans')
-    df.Gender = df.Gender.replace('queer/she/they', 'trans')
-    df.Gender = df.Gender.replace('non-binary', 'trans')
-    df.Gender = df.Gender.replace('nah', 'trans')
-    df.Gender = df.Gender.replace('all', 'trans')
-    df.Gender = df.Gender.replace('enby', 'trans')
-    df.Gender = df.Gender.replace('fluid', 'trans')
-    df.Gender = df.Gender.replace('genderqueer', 'trans')
-    df.Gender = df.Gender.replace('androgyne', 'trans')
-    df.Gender = df.Gender.replace('agender', 'trans')
-    df.Gender = df.Gender.replace('male leaning androgynous', 'trans')
-    df.Gender = df.Gender.replace('guy (-ish) ^_^', 'trans')
-    df.Gender = df.Gender.replace('trans woman', 'trans')
-    df.Gender = df.Gender.replace('neuter', 'trans')
-    df.Gender = df.Gender.replace('female (trans)', 'trans')
-    df.Gender = df.Gender.replace('queer', 'trans')
-    df.Gender = df.Gender.replace('ostensibly male, unsure what that really means', 'trans')
-    df.Gender = df.Gender.replace('p', 'trans')
-    df.Gender = df.Gender.replace('a little about you', 'trans')
+#     df.Gender = df.Gender.replace('trans-female', 'trans')
+#     df.Gender = df.Gender.replace('something kinda male?', 'trans')
+#     df.Gender = df.Gender.replace('queer/she/they', 'trans')
+#     df.Gender = df.Gender.replace('non-binary', 'trans')
+#     df.Gender = df.Gender.replace('nah', 'trans')
+#     df.Gender = df.Gender.replace('all', 'trans')
+#     df.Gender = df.Gender.replace('enby', 'trans')
+#     df.Gender = df.Gender.replace('fluid', 'trans')
+#     df.Gender = df.Gender.replace('genderqueer', 'trans')
+#     df.Gender = df.Gender.replace('androgyne', 'trans')
+#     df.Gender = df.Gender.replace('agender', 'trans')
+#     df.Gender = df.Gender.replace('male leaning androgynous', 'trans')
+#     df.Gender = df.Gender.replace('guy (-ish) ^_^', 'trans')
+#     df.Gender = df.Gender.replace('trans woman', 'trans')
+#     df.Gender = df.Gender.replace('neuter', 'trans')
+#     df.Gender = df.Gender.replace('female (trans)', 'trans')
+#     df.Gender = df.Gender.replace('queer', 'trans')
+#     df.Gender = df.Gender.replace('ostensibly male, unsure what that really means', 'trans')
+#     df.Gender = df.Gender.replace('p', 'trans')
+#     df.Gender = df.Gender.replace('a little about you', 'trans')
 
-    df['Age'] = pd.to_numeric(df['Age'],errors='coerce')
+#     df['Age'] = pd.to_numeric(df['Age'],errors='coerce')
 
-    def age_process(age):
-        if age>=0 and age<=100:
-            return age
-        else:
-            return np.nan
+#     def age_process(age):
+#         if age>=0 and age<=100:
+#             return age
+#         else:
+#             return np.nan
 
-    df['Age'] = df['Age'].apply(age_process)
-
-
-    df = df.dropna(subset=['work_interfere'])
-    df = df.dropna(subset=['Age'])
-    df.copy = df
-
-    # convert binary columns to 0 and 1
-    for col in df.select_dtypes(include=['object']):
-        u_count = len(df[col].unique()) 
-        if u_count == 2:
-            first = list(df[col].unique())[-1]
-            df[col] = (df[col] == first).astype(int)
-            # print('converted', col)
-
-    df.work_interfere = df.work_interfere.map({'Never': 0, 'Rarely': 1,'Sometimes': 2, 'Often': 3})
-
-    df.no_employees = df.no_employees.map({'6-25': 25, '26-100': 100, '100-500':500, '500-1000': 1000, 'More than 1000': 2000, '1-5': 5})
-
-    mapping = {'Yes': 1, 'No': -1, "Don't know": 0,'Not sure': 0, 'Maybe': 0, 'Some of them': 0}
-    #three_factor = {'Yes': 1, 'No': -1, 'Not sure': 0}
-    for col in df.select_dtypes(include=['object']):
-        uniques = set(df[col].unique())
-        if (uniques == {'Yes', 'No', "Don't know"} or
-            uniques == {'Yes', 'No', 'Not sure'} or
-            uniques == {'Yes', 'No', 'Maybe'} or
-            uniques == {'Yes', 'No', 'Some of them'}):
-            # print('converted', col, 'To -1, 0, 1')
-            df[col] = df[col].map(mapping)
+#     df['Age'] = df['Age'].apply(age_process)
 
 
-    df.leave = df.leave.map({'Very easy': 0, 'Somewhat easy': 1, "Don't know": 2, 'Somewhat difficult': 3, 'Very difficult': 4})
+#     df = df.dropna(subset=['work_interfere'])
+#     df = df.dropna(subset=['Age'])
+#     df.copy = df
 
-    df.Gender = df.Gender.map({'male': 1, 'female': -1, 'trans': 0})
+#     # convert binary columns to 0 and 1
+#     for col in df.select_dtypes(include=['object']):
+#         u_count = len(df[col].unique()) 
+#         if u_count == 2:
+#             first = list(df[col].unique())[-1]
+#             df[col] = (df[col] == first).astype(int)
+#             # print('converted', col)
 
-    del df['Country']
+#     df.work_interfere = df.work_interfere.map({'Never': 0, 'Rarely': 1,'Sometimes': 2, 'Often': 3})
 
-    x, y = df.drop('treatment', axis=1), df.treatment
+#     df.no_employees = df.no_employees.map({'6-25': 25, '26-100': 100, '100-500':500, '500-1000': 1000, 'More than 1000': 2000, '1-5': 5})
 
-    model = RandomForestClassifier(n_jobs=-1, n_estimators=10, class_weight='balanced')
+#     mapping = {'Yes': 1, 'No': -1, "Don't know": 0,'Not sure': 0, 'Maybe': 0, 'Some of them': 0}
+#     #three_factor = {'Yes': 1, 'No': -1, 'Not sure': 0}
+#     for col in df.select_dtypes(include=['object']):
+#         uniques = set(df[col].unique())
+#         if (uniques == {'Yes', 'No', "Don't know"} or
+#             uniques == {'Yes', 'No', 'Not sure'} or
+#             uniques == {'Yes', 'No', 'Maybe'} or
+#             uniques == {'Yes', 'No', 'Some of them'}):
+#             # print('converted', col, 'To -1, 0, 1')
+#             df[col] = df[col].map(mapping)
 
-    model.fit(x,y)
-    # print(x.shape)
-    # print(type(x))
-    preds = model.predict(x)
-    print(x.iloc[0:1,:])
-    print(preds[0:2])
+
+#     df.leave = df.leave.map({'Very easy': 0, 'Somewhat easy': 1, "Don't know": 2, 'Somewhat difficult': 3, 'Very difficult': 4})
+
+#     df.Gender = df.Gender.map({'male': 1, 'female': -1, 'trans': 0})
+
+#     del df['Country']
+
+#     x, y = df.drop('treatment', axis=1), df.treatment
+
+#     model = RandomForestClassifier(n_jobs=-1, n_estimators=10, class_weight='balanced')
+
+#     model.fit(x,y)
+#     # print(x.shape)
+#     # print(type(x))
+#     preds = model.predict(x)
+#     print(x.iloc[0:1,:])
+#     print(preds[0:2])
     
-    # save model weights
-    # joblib.dump(model, "./rf_model.joblib")
-    # print("Model trained and exported to joblib")
+#     # save model weights
+#     # joblib.dump(model, "./rf_model.joblib")
+#     # print("Model trained and exported to joblib")
 
-    # print("Feature ranking:")   
-    # for f in range(len(x.columns)):
-    #     print("Feature %s : (%f)" %(x.columns[f], model.feature_importances_[f]))
+#     # print("Feature ranking:")   
+#     # for f in range(len(x.columns)):
+#     #     print("Feature %s : (%f)" %(x.columns[f], model.feature_importances_[f]))
         
 
         
-def model_predict(user_data):
-    # load, no need to initialize the loaded_rf
-    loaded_model = joblib.load("./rf_model.joblib")
-    prediction = loaded_model.predict(user_data)
-    return prediction        
+# def model_predict(user_data):
+#     # load, no need to initialize the loaded_rf
+#     loaded_model = joblib.load("./rf_model.joblib")
+#     prediction = loaded_model.predict(user_data)
+#     return prediction        
         
         
 
