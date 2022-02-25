3c49a4ea28b373b981e08d5e3425cb956dc07777
Ubuntu <ubuntu@ip-172-31-88-101.ec2.internal>
2022-02-25 19:50:44 +0000

    last deps

--- app.py
+++ app.py
@@ -71,31 +71,7 @@ def webhook():
     messaging_text = None
     
     # data = request.json
-    log(data)
-    # if data['object'] == 'page':
-    #     for entry in data['entry']:
-    #         # get all the messages
-    #         messages = entry['messaging']
-    #         if messages[0]:
-    #             # Get the first message
-    #             message = messages[0]
-    #             print("Message**************")
-    #             print(message)
-    #             # Yay! We got a new message!
-    #             # We retrieve the Facebook user ID of the sender
-    #             fb_id = message['sender']['id']
-    #             # We retrieve the message content
-    #             text = message['message']['text']
-    #             print("Collected Info **************")
-    #             print("Sender ID: " + "\'" + fb_id + "\'")
-    #             print("Text: " + text)
-    #             fb_id = ("\'" + fb_id + "\'")
-    #             text = ("\'" + text + "\'")
-    #             # Let's forward the message to Wit /message
-    #             # and customize our response to the message in handle_message
-    #             response = client.message(msg=text, context={'session_id':fb_id})
-    #             handle_message(response=response, fb_id=fb_id)
-     
+    log(data)   
     
     if data['object']=='page':
         for entry in data['entry']:
@@ -123,26 +99,6 @@ def webhook():
                             messaging_text = 'no text'
                             fb_message(sender_id, messaging_text)
                 
-                    # #Echo bot
-                    # response = messaging_text
-                    # response = client.message(messaging_text)
-                    # response = chatbot(messaging_text)
-                    # bot.send_text_message(sender_id, response)
-                    # print("JSON Request Data :")
-                    # print(data)
-                    # print("Sender ID :")
-                    # print(sender_id)
-                    # print("Message Text :")
-                    # print(messaging_text)
-                    # print("#"*30)
-                    # print("(msg=" + messaging_text +","+ "context={'session_id':" + sender_id + "})")
-                    # response = client.message(msg=messaging_text, context={'session_id':sender_id})
-                    # print("Beyond response")
-                    # response = chatbot(messaging_text)
-                    # fb_message(sender_id, response)
-                    # bot.send_text_message(sender_id, response)
-                    # handle_message(response=response, fb_id=sender_id)
-                
     else:
         # Returned another event
         return 'Received Different Event'
@@ -152,6 +108,7 @@ def webhook():
 def chatbot(sender_id,txt):
     global allval, od, x
     counter=0
+    tup = ()
     file_name = sender_id + ".csv"
     # response = "Some response received from Wit"
     intent, entity, value = wit_response(txt)
@@ -164,14 +121,9 @@ def chatbot(sender_id,txt):
         rdf[counter+1] = [value]
         rdf.to_csv(file_name, sep=",", index=False)
         print("----SURVEY FILE UPDATED----SURVEY FILE UPDATED-----")
-    
-    # allval[x] = value
-    # Append to CSV here
-    # x = x + 1
-    tup = ()
  
     if intent == 'greetings':
-        response = "Hi, Welcome to My Mental Health app! We will do a small survey to predict how work related stress could be affecting your mental health. Shall we begin?"
+        response = "Hi, Welcome to My Mental Health app!\nWe will do a small survey to predict how work related stress could be affecting your mental health.\nShall we begin?"
 
     elif intent == 'confirmation':
         if entity == 'yes_no' and value == 'yes':
@@ -192,23 +144,14 @@ def chatbot(sender_id,txt):
             print("Exit with keyword")
             response = "See you later!!"
         else:
-            pass
+            response = "Unrecognized response received, start with a 'Hey' again!"
            
     
     elif entity == 'wit$number' and counter < 22:
-        # global count
         print(counter, "-------IN QUESTION LIST NOW-----------")
         response = od[counter]
-        # print(count, " in Question list")
-        # response = od[count]
-        # count = count + 1
-        # print(allval)
     elif counter == 22:
         print("----SURVEY ENDED-----")
-        # allval.pop(0)
-        # allval.pop(1)
-        # for key, value in allval.items():
-        #     tup = tup + (value,)
         if path.exists(file_name):
             fdf = pd.read_csv(file_name, sep=",")
             tup = list(fdf.itertuples(index=False, name=None))[0]
@@ -224,45 +167,6 @@ def chatbot(sender_id,txt):
             response = "Survery File Not Found!!"
     else:
         response = "Exit due to error"
-      
-    
-    
-    # elif intent == 'info' and start_flag == 1:
-    #     if entity == 'wit$number' and value.isnumeric():
-    #         if value < 10 and value > 80:
-    #             response = "You are either too young or too old!!"
-            
-#             global count
-#             print(count, " in Question list")
-#             response = od[count]
-#             count = count + 1
-#             print(allval)
-    
-#     elif len(allval) == 24:
-#           print("reached the end!")
-#           allval.pop(0)
-#           allval.pop(1)
-#           for key, value in allval.items():
-#             tup = tup + (value,)
-#           outcome = predict(tup)
-#           response = "The outcome is {}".format(str(outcome))
-    
-    
-    # elif entity == 'number' and len(allval) < 24:
-    #       #value > -1 and value < 100:
-    #       global count
-    #       print(count, " in Question list")
-    #       response = od[count]
-    #       count = count + 1
-    #       print(allval)
-    # elif len(allval) == 24:
-    #       print("reached the end!")
-    #       allval.pop(0)
-    #       allval.pop(1)
-    #       for key, value in allval.items():
-    #         tup = tup + (value,)
-          # outcome = predict(tup)
-          # response = "The outcome is {}".format(str(outcome))
      
     return response
 
@@ -282,31 +186,6 @@ def fb_message(sender_id, text):
                          json=data)
     print("-----MESSAGE POSTED FROM BOT------")
     return resp.content    
-    
-    
-
-def handle_message(response, fb_id):
-    """
-    Customizes our response to the message and sends it
-    """
-    # Checks if user's message is a greeting
-    # Otherwise we will just repeat what they sent us
-    # greetings = first_trait_value(response['traits'], 'wit$greetings')
-    # if greetings:
-    #     text = "hello!"
-    # else:
-    #     text = "We've received your message: " + response['_text']
-    # # send message
-    # fb_message(fb_id, text)
-    # print("################## IN HANDLE_MESSAGE ###################")
-    # intent, entity, value = wit_response(response)
-    # if intent == 'greetings':
-    #     text = "Hi, Welcome to My Mental Health app! We will do a small survey to predict how work related stress could be affecting your mental health. Shall we begin?"
-    # else:
-    #     text = "We've received your message: " + response['_text']
-    # send message
-    # text = "We've received your message: " + response['text']
-    # fb_message(fb_id, text)
             
     
     
