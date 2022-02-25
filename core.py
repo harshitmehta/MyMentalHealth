91eec025c2a3e714fd4da4a1af9f8f7c68666e30
Ubuntu <ubuntu@ip-172-31-88-101.ec2.internal>
2022-02-25 23:41:15 +0000

    fixing bugs

--- core.py
+++ core.py
@@ -127,7 +127,12 @@ from sklearn.ensemble import RandomForestClassifier
 def model_predict(user_data):
     # load, no need to initialize the loaded_rf
     print("Inside Model Predict")
+    print(type(user_data))
+    print("---The user data received from app---")
+    print(user_data)
     loaded_model = joblib.load("./new_rf_model.joblib")
+    print("Joblib successfully loaded")
+    
     prediction = loaded_model.predict(user_data)
     return prediction        
         
