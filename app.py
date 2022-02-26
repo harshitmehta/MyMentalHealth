1636a0c3003f6aeab21b331a811b62dc3d6f703b
Ubuntu <ubuntu@ip-172-31-88-101.ec2.internal>
2022-02-26 01:37:20 +0000

    final changes

--- app.py
+++ app.py
@@ -52,34 +52,8 @@ od[19] = 'Do you feel that your employer takes mental health as seriously as phy
 od[20] = 'Have you heard of or observed negative consequences for coworkers with mental health conditions in your workplace?\n0 : no \n1 : yes '
 od[21] = 'Thanks! Calculating... \nSend \'9\' to see your result'
 
-###### Old Information
-# od = OrderedDict()
-# od[0] = 'What is your age?'
-# od[1] = 'What is your gender? 1 : Male 0 : Transgender -1 : Female'
-# od[2] = 'Do you have a family history of mental illness? 1 : yes 0 : no'
-# od[3] = 'If you have a mental health condition, do you feel that it interferes with your work? 0 : never 1 : rarely 2 : sometimes 3 : often'
-# od[4] = 'How many employees does your company or organization have? 1 : 1-5 2 : 6-25 3 : 26-100 4 : 100-500 5 : 500-1000 6: More than 1000'
-# od[5] = 'Do you work remotely (outside of an office) at least 50% of the time? 1 : yes 0 : no'
-# od[6] = 'Is your employer primarily a tech company/organization? 1 : yes 0 : no'
-# od[7] = 'Does your employer provide mental health benefits? 1 : yes 0 : don\'t know -1 : no'
-# od[8] = 'Do you know the options for mental health care your employer provides? 1 : yes 0 : not sure -1 : no'
-# od[9] = 'Has your employer ever discussed mental health as part of an employee wellness program? 1 : yes 0 : don\'t know -1 : no'
-# od[10] = 'Does your employer provide resources to learn more about mental health issues and how to seek help? 1 : yes 0 : don\'t know -1 : no'
-# od[11] = 'Is your anonymity protected if you choose to take advantage of mental health or substance abuse treatment resources? 1 : yes 0 : don\'t know -1 : no'
-# od[12] = 'How easy is it for you to take medical leave for a mental health condition? 0 : very easy 1 : somewhat easy 2 : don\'t know 3 : somewhat difficult 4 : very difficult'
-# od[13] = 'Do you think that discussing a mental health issue with your employer would have negative consequences? 1 : yes 0 : maybe -1 : no'
-# od[14] = 'Do you think that discussing a physical health issue with your employer would have negative consequences? 1 : yes 0 : maybe -1 : no'
-# od[15] = 'Would you be willing to discuss a mental health issue with your coworkers? 1 : yes 0 : some of them -1 : no'
-# od[16] = 'Would you be willing to discuss a mental health issue with your direct supervisor(s)? 1 : yes 0 : some of them -1 : no'
-# od[17] = 'Would you bring up a mental health issue with a potential employer in an interview?1 : yes 0 : maybe -1 : no'
-# od[18] = 'Would you bring up a physical health issue with a potential employer in an interview? 1 : yes 0 : maybe -1 : no'
-# od[19] = 'Do you feel that your employer takes mental health as seriously as physical health? 1 : yes 0 : don\'t know -1 : no'
-# od[20] = 'Have you heard of or observed negative consequences for coworkers with mental health conditions in your workplace? 1 : yes 0 : no'
-# od[21] = 'Thanks! Calculating...\nSend \'9\' to see your result'
-
-
-allval = OrderedDict()
-x = 0
+# allval = OrderedDict()
+# x = 0
 
 
 @app.route('/', methods=['GET'])
@@ -133,7 +107,7 @@ def webhook():
     return "ok", 200
 
 def chatbot(sender_id,txt):
-    global allval, od, x
+    global od
     counter=0
     tup = ()
     file_name = sender_id + ".csv"
